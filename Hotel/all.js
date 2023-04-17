0029ae2896040faa35c5f78775c2e1f7f7c587df
RafaGR <110624346+RaffaGR@users.noreply.github.com>
2023-04-16 23:24:24 -0300

    Correct inversion

--- Hotel/all.js
+++ Hotel/all.js
@@ -340,24 +340,25 @@ var nomeUsuario;
 
         // carros do hotel - GasolinaAlcolica
         function CH7() {
+/* 
             var vAW = parseFloat(prompt("Qual o valor do álcool no posto Wayne Oil?"));
             var vGW = parseFloat(prompt("Qual o valor da gasolina no posto Wayne Oil?"));
             var vAS = parseFloat(prompt("Qual o valor do álcool no posto Stark Petrol?"));
             var vGS = parseFloat(prompt("Qual o valor da gasolina no posto Stark Petrol?"));
-            /* var vAW = 4.20
-            var vGW = 5.82
-            var vAS = 4.35
-            var vGS = 6.17 */
+            // var vAW = 4.20
+            // var vGW = 5.82
+            // var vAS = 4.35
+            // var vGS = 6.17
 
             // 2 tentativas com regras - regra de três para calcular qual combustível é mais vantajoso em cada posto
             // 42 = lT = litros total --- l = lA = plA = preço litro alcool --- vAW = pA = preço alcool --- vGW = pG = preço gasolina
-            // var lAW = (42 * vAW) / pTA; // falta o valor de litros da grandeza 2, não conseguiremos calcular usando a regra de 3!!!
+            // var lAW = (42 * vAW) / pTA; // faltaria 3 grandeza
             // poderiamos verificar qual combustivel é o mais em conta usando 70% do valor da gasolina, ou seja, use a comparação g*0.70 - pelo fato da gasolina estar 30% mais cara que o alcool tiramos isso dela e então comparamos
 
 
             // primeira tentativa sem regras - sentido inverso, pelo fato do alcool estar 30% mais barato, adicionamos 30% nela e então comparamos
             var gVSa = "";
-            divino = "";
+            var divino = "";
             var daGloria1 = 0;
             if (vAW + vAW * 0.30 > vGW) {
                 daGloria1 = vAW + vAW * 0.30
@@ -376,13 +377,56 @@ var nomeUsuario;
             }
             if (daGloria1 < daGloria2) {
                 deGracaDivina = daGloria2;
-                divino = "Stark Petrol";
+                divino = "Wayne Oil";
             } else {
                 deGracaDivina = daGloria1;
-                divino = "Wayne Oil";
+                divino = "Stark Petrol";
             }
 
-            alert(`${nomeUsuario}, é mais barato abastecer com ${gVSa} no posto ${divino}`);
+            alert(`${nomeUsuario}, é mais barato abastecer com ${gVSa} no posto ${divino} ? metodo invertido, o mais barato é o mais caro ${deGracaDivina}`);
+             */ //problema, estava invertido mais funcionava
+
+            //Corrigido
+            var vAW = parseFloat(prompt("Qual o valor do álcool no posto Wayne Oil?"));
+            var vGW = parseFloat(prompt("Qual o valor da gasolina no posto Wayne Oil?"));
+            var vAS = parseFloat(prompt("Qual o valor do álcool no posto Stark Petrol?"));
+            var vGS = parseFloat(prompt("Qual o valor da gasolina no posto Stark Petrol?"));
+
+            var gVSa = "";
+            var divino = "";
+            var daGloria1 = 0;
+            if (vAW > vGW * 0.70) {
+                daGloria1 = vAW;
+                gVSa = "álcool";
+            } else {
+                daGloria1 = vGW
+                gVSa = "gasolina";
+            }
+            var daGloria2 = 0;
+            if (vAS > vGS * 0.70) {
+                daGloria2 = vAS;
+                gVSa = "álcool";
+            } else {
+                daGloria2 = vGS;
+                gVSa = "gasolina";
+            }
+            if (daGloria1 < daGloria2) {
+                var deGracaDivina = daGloria1;
+                divino = "Wayne Oil";
+            } else {
+                deGracaDivina = daGloria2;
+                divino = "Stark Petrol";
+            }
+
+            //Regra de 3 com 2 grandezas
+            // var vASdoCarro = (vAS * 1) * 42;
+            // var vGSdoCarro = (vGS * 1) * 42;
+            // var vAWdoCarro = (vAW * 1) * 42;
+            // var vGWdoCarro = (vGW * 1) * 42;
+
+            // Não precisa da Regra de 3, por que não precisa mostrar o preço total
+            
+            alert(`${nomeUsuario}, é mais barato abastecer com ${gVSa} no posto ${divino} = valor total ${deGracaDivina}`);
         }
 
         // climatização no hotel - ultimate acnalisy
